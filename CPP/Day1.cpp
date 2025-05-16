b835a5caa764334a3ac4e0bea3d9b111bb0eebe8
Ashish Ranjan <148757537+AshShubh72@users.noreply.github.com>
2025-05-16 18:44:21 +0530

    Did day 1 experiment
    tried to solved the compilor issue

---
+++ CPP/Day1.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     cout << "Ashish ranjan" << endl;
+//     cout << " i dont know " << endl;
+//     return 0;
+// }
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    std::cout << "Number3" << std::endl;
+    return 0;
+}
